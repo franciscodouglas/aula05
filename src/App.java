ee4e01bced8f01be25d79ca9c8ec60ffbe6876bb
Francisco Douglas <98854868+franciscodouglas@users.noreply.github.com>
2024-03-20 12:13:48 -0300

    Meu terceiro commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Ola, Mundo!");
+        System.out.println("Ola, Mundo! 20/03/2024");
     }
 }
